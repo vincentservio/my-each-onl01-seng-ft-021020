1711ec8e43cfa9c5471338f250bb7d852f3e3cbb
Vincent Servio <vincentservio@gmail.com>
2020-02-12 23:09:45 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -10,11 +10,3 @@ def my_each(array)
 end
 
 
-# The "while" method will always return a value of => nil"
-    yield(array[i])
-    i = i + 1
-  end
-end
-
-my_each(tas) { |ta| puts ta}
->>>>>>> f44a58b4db0cd035fdebf05c68d442bbf5504bd2
